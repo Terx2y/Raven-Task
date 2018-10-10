e0809704625fc373e7ca768987c7fd137aa5ea7e
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-10 16:52:39 +0800

    App routes added

--- routes.php
+++ routes.php
@@ -3,3 +3,12 @@
  * Our app navigation
  */
 
+use app\Router as route;
+$routes = new app\Router();
+
+$routes->get("/", "MainController@index");
+$routes->get("/tasks", "TasksController@index");
+$routes->post("/add", "TasksController@create");
+$routes->get("/favorites", "FavoriteController@getList");
+
+route::fetch();
