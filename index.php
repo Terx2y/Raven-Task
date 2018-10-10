dd73d2cd9230d331379f5271a202f73e06276475
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-10 16:52:16 +0800

    Error reporting ON

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    // App entry point
+// App entry point
+    error_reporting( E_ALL );
+
     spl_autoload_register();
 
     require "vendor/autoload.php";
-    require_once "app/routes.php";
+    require_once "routes.php";
