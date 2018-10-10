5be659cd6913652976e98dc6dc80caf7ebb699d4
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-10 17:10:19 +0800

    PHP Console for this app (primary)

---
+++ Bender.php
@@ -0,0 +1,24 @@
+<?php
+
+spl_autoload_register();
+$console = new \app\Console\BenderLogic();
+
+if ($argv[1] == "-make")
+{
+    if(!empty($argv[2])) {
+        switch ($argv[2]) {
+            case 'user:':
+                (!empty($argv[3]) ? $console->make($argv[3] . " " . $argv[4], "user") : exit("Argument " . $argv[2] . " should have value"));
+                break;
+            default:
+                exit("Unknown parameter: " . $argv[2]);
+                break;
+        }
+    }
+    else{
+        exit("\n\tBender usage: -command [-make] parameter: [user:] name\n");
+    }
+}
+else{
+    exit("Unknown command: " . "'" . $argv[1] . "'");
+}
---
+++ app/Console/BenderLogic.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\Console;
+use app\OuterWorld\DataBase;
+
+class BenderLogic
+{
+    private $user = []; // Here we'll keep our user data
+    private $modelName = []; // Here we'll keep our model data
+    private $controllerName = []; // Here we'll keep our controller data
+
+    public function __construct()
+    {
+        DataBase::initialize(); // Инициализация работы с БД
+    }
+
+    public function __call($name, $arguments)
+    {
+        if($arguments[1] == "user")
+        {
+            $this->user = ["name" => $arguments[0]];
+            self::setPassword($this->user);
+        }
+    }
+
+    public static function setPassword($user)
+    {
+
+        echo "Here we will generate password for " . $user['name'];
+    }
+}
