6cf2bd07823cf80216530312d8151fc308fd6fe6
Terx2y <r4n3@mail.ru>
2018-10-15 00:50:42 +0800

    Validator added

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -1,9 +1,43 @@
 <?php
+use app\Models\Task;
 
 class TasksController extends \app\Http\Controllers\Controller
 {
     public function index()
     {
-        echo 'Tasks page';
+        $tasks = (new Task())->all(); // Here we get all data from "tasks" table
+
+        self::make('tasks', [
+            'success' => $tasks['success']
+        ]);
+
+        unset($tasks);
     }
+
+    public function create()
+    {
+        $validate = new \app\Validator();
+        $data = [
+            'name' => htmlspecialchars($_POST['Name']),
+            'description' => htmlspecialchars($_POST['Desc']),
+            'priority' => htmlspecialchars($_POST['priority']),
+            'deadline' => htmlspecialchars($_POST['deadline'])
+        ];
+
+        $result = $validate->check($data, [
+            'name' => 'required|min_len,6',
+            'description' => 'required|min_len,6|max_len,2048'
+        ]);
+
+        if($result)
+        {
+            $task = new Task();
+            if($task->create($data) == true)
+            {
+                echo 'Задача успешно создана';
+            }
+            unset($task);
+        }
+    }
+
 }
