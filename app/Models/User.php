8a7d3e66dc1bdfd0546f264f33932786e53342c2
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-16 17:36:30 +0800

    Check if user exists

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,9 +1,24 @@
 <?php
-
 namespace app\Models;
 
 class User extends BaseModel
 {
     protected $table = "users";
     public $fillable = ['name','email','password'];
+    public $userData = array();
+
+    public function checkUser($value)
+    {
+            $dataUser = array();
+            $readFromDB = $this->pdo->query("SELECT * FROM " . $this->table . " WHERE `email` = " . "\"" . $value . '"');
+
+            foreach ($readFromDB as $r)
+            {
+                $dataUser['id'] = $r['id'];
+                $dataUser['name'] = $r['name'];
+                $dataUser['email'] = $r['email'];
+                $dataUser['password'] = $r['password'];
+            }
+            return $dataUser;
+    }
 }
