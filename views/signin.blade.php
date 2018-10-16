be95952beb0ce76ecc0171ca15466a6a87ce7487
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-16 17:42:09 +0800

    Added error listing

--- views/signin.blade.php
+++ views/signin.blade.php
@@ -2,9 +2,25 @@
 @section('title', 'Вход')
 
 @section('content')
+
     <form action="/welcome-back" method="POST">
-        <input type="text" name="login" placeholder="Email">
-        <input type="password" name="password" placeholder="Пароль">
+        <div class="fields">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Пароль">
+        </div>
+
         <input type="submit" value="Войти">
     </form>
+    <p class="retrieve-password">
+        <a href="/retrieve">Забыли пароль?</a>
+    </p>
+
+    @if(isset($errors) && count($errors) > 0)
+        <div class="errors">
+            @foreach($errors as $error)
+                <p> {!! $error !!} </p>
+            @endforeach
+        </div>
+    @endif
+
 @endsection
