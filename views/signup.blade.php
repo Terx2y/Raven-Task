6babdedb71db6200df428fb8d81682aa2b3b1470
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-16 17:43:11 +0800

    I won't make this form

--- views/signup.blade.php
+++ views/signup.blade.php
@@ -3,10 +3,12 @@
 
 @section('content')
     <form action="/welcome" method="POST">
-        <input type="text" name="login" placeholder="Имя">
-        <input type="email" name="email" placeholder="EMail">
-        <input type="password" name="password" placeholder="Пароль">
-        <input type="password" name="confirm" placeholder="Повторите пароль">
+        <div class="fields-register">
+            <input type="text" name="login" placeholder="Имя">
+            <input type="email" name="email" placeholder="EMail">
+            <input type="password" name="password" placeholder="Пароль">
+            <input type="password" name="confirm" placeholder="Повторите пароль">
+        </div>
         <input type="submit" value="Регистрация">
     </form>
 @endsection
