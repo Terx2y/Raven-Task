d208fbe04687f71ba125da399410a826c3c1b85d
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-16 17:41:12 +0800

    Added CSS for errors and login form

--- views/template/user.blade.php
+++ views/template/user.blade.php
@@ -40,11 +40,115 @@
             text-decoration: underline;
         }
         .wrapper{
-            width: 960px;
-            height: 500px;
+            padding: 100px;
+            height: auto;
             box-shadow: 0 2px 4px 0 rgba(0,0,0,0.13);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
         }
 
+        .wrapper .sign-logo{
+            padding-bottom: 60px;
+            font-family: 'Ubuntu';
+            color: #444444;
+            font-size: 3em;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .wrapper .sign-logo span{
+            font-family: 'Ubuntu';
+            color: #444444;
+            font-size: 15px;
+            margin-left: 95px;
+        }
+        .wrapper form{
+            width: 400px;
+            height: 180px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+        }
+
+        .wrapper .retrieve-password{
+            margin-top: 50px;
+        }
+        .wrapper .retrieve-password a{
+            font-family: 'Ubuntu';
+            color: #386E93;
+            text-decoration: none;
+        }
+
+        .wrapper .retrieve-password a:hover{
+            transition: color ease 0.5s;
+            color: #254962;
+        }
+
+        .wrapper .join{
+            margin-top: 20px;
+        }
+        .wrapper .join a{
+            font-family: 'Ubuntu';
+            color: #386E93;
+            text-decoration: none;
+        }
+
+        .wrapper .join a:hover{
+            transition: color ease 0.5s;
+            color: #254962;
+        }
+
+        .fields{
+            display: flex;
+            width: 100%;
+            height: 100px;
+            flex-direction: column;
+            justify-content: space-around;
+        }
+        .fields input[type=email], input[type=password], input[type=text]{
+            height: 35px;
+            width: inherit;
+            padding: 0 0 0 15px;
+            outline: none;
+            font-family: 'Ubuntu';
+            font-size: 16px;
+            border-radius: 20px;
+            border: 1px #4444 solid;
+        }
+        input[type=submit]{
+            height: 35px;
+            width: 220px;
+            border: none;
+            outline: none;
+            cursor: pointer;
+            font-family: 'Ubuntu';
+            font-size: 16px;
+            background-color: #007ACC;
+            color: #fff;
+            margin-bottom: -20px;
+            border-radius: 20px;
+        }
+
+        input[type=submit]:hover{
+            transition: background-color ease 0.5s;
+            background-color: #254962;
+        }
+
+        .errors{
+            margin: 60px 0 -60px 0;
+            background-color: rgba(255,41,44,0.2);
+            width: 100%;
+            height: auto;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            color: #ff292c;
+            border: 1px #ff292c solid;
+        }
     </style>
 </head>
 <body>
@@ -54,6 +158,10 @@
     </p>
 </header>
     <div class="wrapper">
+        <p class="sign-logo">
+            Raven Task
+            <span>Планировщик задач</span>
+        </p>
         @yield('content')
     </div>
 </body>
