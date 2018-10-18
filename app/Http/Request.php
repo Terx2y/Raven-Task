159d4c013c331df2973178a3b5bd90411bf83d2f
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-18 16:08:21 +0800

    isPost method addad

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -2,10 +2,17 @@
 
 namespace app\Http;
 
-
 class Request
 {
+    public $request;
+
     public function redirect($uri){
         header('Location: ' . $uri);
+    }
+
+    public function isPost()
+    {
+        (($_SERVER['REQUEST_METHOD'] == "POST") ? $this->request = true : $this->request = false);
+        return $this->request;
     }
 }
