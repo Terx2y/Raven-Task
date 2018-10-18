6dce6ed66bfe4fc219f04393182043bbaf5fd289
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-10-18 16:09:50 +0800

    Routes cleared

--- routes.php
+++ routes.php
@@ -10,9 +10,7 @@ $routes->get("/", "MainController@index");
 $routes->get("/tasks", "TasksController@index");
 $routes->post("/add", "TasksController@create");
 $routes->get("/favorites", "FavoriteController@getList");
-
 $routes->get("/welcome-back", "UserController@signin");
-//$routes->get("/welcome", "UserController@signup");
 $routes->get("/logout", "UserController@logout");
 
 route::fetch();
