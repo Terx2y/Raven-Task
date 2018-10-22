f1acacca08713a5d3ba6d43a07c3573e0f424070
Terx2y <r4n3@mail.ru>
2018-10-23 01:01:08 +0800

    Here we recieve user data array

--- views/main.blade.php
+++ views/main.blade.php
@@ -5,23 +5,11 @@
 @section('content')
 
     <div id="tasks_content">
-        <p style="width: 100%; display: flex; justify-content: flex-start; align-items: center; height: 62px; font-size: 1.5em; padding-left: 10px;">Добро пожаловать, {{$success['name']}} <span><a
-                        href="/logout">(выйти)</a></span></p>
+
+        <p style="width: 100%; display: flex; justify-content: flex-start; align-items: center; height: 62px; font-size: 1.5em; padding-left: 10px;">Добро пожаловать, {{ $user['name'] }}
+            <span> <a href="/logout">&nbsp;&nbsp;выйти</a></span>
+        </p>
         <div id="list_tasks" class="dashboard display-animation" style="margin: 0 auto;">
-            {{--@forelse($success as $task)--}}
-                {{--<a class="{{ $task['priority'] }}" href="#">--}}
-                    {{--<span class="content-wrapper">--}}
-                      {{--<span class="tile-content">--}}
-                        {{--<span class="tile-img" style="background-image: url(https://www.google.com/design/images/googleplus.png;)"></span>--}}
-                        {{--<span class="tile-holder tile-holder-sm">--}}
-                          {{--<span class="title">{{ $task['name'] }}</span>--}}
-                        {{--</span>--}}
-                      {{--</span>--}}
-                    {{--</span>--}}
-                {{--</a>--}}
-            {{--@empty--}}
-                {{--<p>Список заданий пуст</p>--}}
-            {{--@endforelse--}}
         </div>
 
     </div>
